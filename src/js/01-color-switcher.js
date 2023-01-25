2606d7f99ee0653b6a39d0d1264278c11092880c
dire <example@gmail.com>
2023-01-25 19:36:36 +0200

    first task complitte

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,35 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+const body = document.querySelector('body');
+
+startBtn.addEventListener("click", changeBackgroundColor);
+stopBtn.addEventListener("click", colorChangeStop);
+
+function changeBackgroundColor(){
+	startBtn.disabled = true;
+	intervalAdd();
+}
+
+function intervalAdd(){
+	timerId = setInterval(() => { 
+		setBackgroundColor()
+	  }, 1000);
+}
+
+function colorChangeStop(){
+	timerReset();
+	startBtn.disabled = false;
+}
+
+function timerReset(){
+	clearInterval(timerId);
+}
+
+function setBackgroundColor(){
+	body.style.backgroundColor = getRandomHexColor();
+}
 
 function getRandomHexColor() {
-	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }ç
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
